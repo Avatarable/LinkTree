1fdc4c3c37de67b1224eb12486c18e70460c3a46
Able <avatarable@gmail.com>
2022-11-01 08:00:33 +0100

    changed profile pic and added slack username

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,8 @@ function Profile() {
         <div className="img__div">
             <img src={require("../assets/images/profile__img.png")} alt="" id="profile__img" />
         </div>
-        <p className="username" id="twitter">Annette Black</p>
+        <p className="username" id="twitter">Avatar_Able</p>
+        <p id='slack' hidden>Avatarable</p>
     </div>
   )
 }
